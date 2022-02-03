df9b027c0be934103e1ae94648ecbdcca35fa232
victor.longon <vhlongon@gmail.com>
2022-02-03 17:38:59 +0100

    create story with args

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
 
-const Home: NextPage = () => {
+const Home: NextPage<{ name?: string }> = ({ name }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js! - {name}</a>
         </h1>
 
         <p className={styles.description}>
@@ -35,16 +35,14 @@ const Home: NextPage = () => {
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+            className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+            className={styles.card}>
             <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -57,8 +55,7 @@ const Home: NextPage = () => {
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
@@ -66,7 +63,7 @@ const Home: NextPage = () => {
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- stories/pages/Home.stories.tsx
+++ stories/pages/Home.stories.tsx
@@ -1,8 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import HomePage from '../../pages/index';
 
 export default {
   title: 'Pages/Home',
   component: HomePage,
-};
+  args: {
+    name: 'All Stories default',
+  },
+} as ComponentMeta<typeof HomePage>;
 
-export const Home = () => <HomePage />;
+const Template: ComponentStory<typeof HomePage> = args => (
+  <HomePage {...args} />
+);
+
+export const Home = Template.bind({});
+
+Home.args = {
+  name: 'Home story default',
+};
