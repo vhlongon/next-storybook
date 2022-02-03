3b0903c56208afe13f46629963d7e2e4a8aaee29
victor.longon <vhlongon@gmail.com>
2022-02-03 17:44:59 +0100

    serve assets in public map from next in storybook and patch next image component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 import '../styles/globals.css';
+import * as NextImage from 'next/image';
+
+const OriginalImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: props => <OriginalImage {...props} unoptimized />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
